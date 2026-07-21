7e957d6333cc8d2ef2dc2a143670464572c2d24b
tobungle <tsan03@outlook.com>
2026-07-21 19:03:43 +0100

    Changed camera sens & mouse lock

--- player/Player.cs
+++ player/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 
 public partial class Player : CharacterBody3D
@@ -6,7 +7,7 @@ public partial class Player : CharacterBody3D
 	float speed = 8.0f;
 	float jump_force = 15.0f;
 	float gravity = 0.5f;
-	float cam_rot_sensitivity = 0.05f;
+	float cam_rot_sensitivity = 0.5f;
 	Vector2 mouse_relative;	// Where is mouse, relative to last frame. Basically how much it's moving
 	public override void _Ready()
 	{
@@ -34,16 +35,27 @@ public partial class Player : CharacterBody3D
 	void DoCamera()
 	{
 		bool rotating = Input.IsActionPressed("rotate_camera");
+		// Update mouse visibility & lock based on whether we're rotating or not
+		if (rotating)
+		{
+			Input.MouseMode = Input.MouseModeEnum.Captured;
+		}
+		else
+		{
+			Input.MouseMode = Input.MouseModeEnum.Visible;
+		}
 		// Escape from function if not holding down rotate button or if the mouse isn't moving
 		if (!rotating || mouse_relative == Vector2.Zero)
 		{
 			return;
 		}
 		// Modify camera rotation based on mouse relative
-		Vector3 cam_rot = camera_base.Rotation;
+		Vector3 cam_rot = camera_base.RotationDegrees;
 		cam_rot.Y -= mouse_relative.X * cam_rot_sensitivity;
 		cam_rot.X -= mouse_relative.Y * cam_rot_sensitivity;
-		camera_base.Rotation = cam_rot;
+		// Clamp mouse angle to prevent going upside down
+		cam_rot.X = Math.Clamp(cam_rot.X, -80f, 80f);
+		camera_base.RotationDegrees = cam_rot;
 	}
 	
 	void DoMovement()
