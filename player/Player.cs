2306017712943c5ea853709f99363a099d157dea
tobungle <tsan03@outlook.com>
2026-07-21 17:51:49 +0100

    Added GodotSteam

---
+++ player/Player.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public partial class Player : Node3D
+{
+	
+	public override void _Ready()
+	{
+		
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		
+	}
+	
+}
