5675a6acdb696cf02cf2a010c1e5c0610a4eea18
tobungle <tsan03@outlook.com>
2026-07-24 23:44:56 +0100

    Fixed sync issue with RemotePlayer.cs i didnt filter for player id hee hee

--- scenes/remote_player/RemotePlayer.cs
+++ scenes/remote_player/RemotePlayer.cs
@@ -1,20 +1,28 @@
 using Godot;
+using System;
 
 public partial class RemotePlayer : Node3D
 {
 	Node network;
+	int id;
 	public override void _Ready()
 	{
+		// When a remote player is spawned, their id is assigned as the node name
+		id = Convert.ToInt32(Name);
+		// Get network autload
 		network = GetNode<Node>("/root/Network");
 		// Connect the signal to a lambda which passes the argument to OnPlayerSync
 		// Why cant i just do this directly? Dont fucking know Fuck you Eat shit
-        network.Connect("on_player_synced", Callable.From((Vector3 new_position) => OnPlayerSync(new_position)));
+        network.Connect("on_player_synced", Callable.From((int player_id, Vector3 new_position) => OnPlayerSync(player_id, new_position)));
 	}
 
 	// Function that syncs player on client
 	// Right now just syncs position
-	void OnPlayerSync(Vector3 new_position)
+	void OnPlayerSync(int player_id, Vector3 new_position)
 	{
-		GlobalPosition = new_position;
+		if (player_id == id)
+		{
+			GlobalPosition = new_position;
+		}
 	}
 }
