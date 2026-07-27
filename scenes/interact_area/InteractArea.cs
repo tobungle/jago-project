bec3244e6448a4c32be90d58d401ad276ad237ca
tobungle <tsan03@outlook.com>
2026-07-27 14:32:34 +0100

    Changed interactions to use camera raycast Some errors are thrown to do with disposed objects being accessed, i will fix this

--- scenes/interact_area/InteractArea.cs
+++ scenes/interact_area/InteractArea.cs
@@ -3,36 +3,10 @@ using Godot;
 public partial class InteractArea : Area3D
 {
 	public Interactable interactable;
-	Globals globals;
 	public override void _Ready()
 	{
-		globals = GetNode<Globals>("/root/Globals");
-		MouseEntered += OnMouseEntered;
-		MouseExited += OnMouseExited;
-
 		// This bit of code will break if parent is not an interactable
 		Node parent = GetParent<Node>();
-		// Cleanup if parent is deleted
-		parent.TreeExiting += () =>
-		{
-			if (globals.hovered_interactable == parent)
-			{
-				globals.hovered_interactable = null;
-			}
-		};
 		interactable = parent as Interactable;
-	}
-
-	void OnMouseEntered()
-	{
-		globals.hovered_interactable = interactable;
-	}
-
-	void OnMouseExited()
-	{
-		if (globals.hovered_interactable == interactable)
-		{
-			globals.hovered_interactable = null;
-		}
 	}
 }
--- scenes/player/InteractionManager.cs
+++ scenes/player/InteractionManager.cs
@@ -1,9 +1,10 @@
 using Godot;
-using System;
 
-public partial class InteractionManager : Node
+public partial class InteractionManager : Node3D
 {
+	[Export] Camera3D camera;	// Needed for camera raycast
 	Globals globals;
+	const float ray_length = 300f;
 	public override void _Ready()
 	{
 		globals = GetNode<Globals>("/root/Globals");
@@ -17,8 +18,43 @@ public partial class InteractionManager : Node
 		}
 	}
 
+	public override void _PhysicsProcess(double delta)
+	{
+		RaycastFromCamera();
+	}
+
 	void OnGetItemPressed()
 	{
 		globals.hovered_interactable?.Interact();
+	}
+
+	void RaycastFromCamera()
+	{
+		// Project ray from camera to infront of camera
+		// Thank you random stack overflow user
+		Vector2 mouse_pos = GetViewport().GetMousePosition();
+		Vector3 origin = camera.ProjectRayOrigin(mouse_pos);
+		var end = origin + camera.ProjectRayNormal(mouse_pos) * ray_length;
+
+		// Do le raycast
+		var query = PhysicsRayQueryParameters3D.Create(origin, end);
+		query.CollideWithAreas = true;
+		// Mask layer 4 (items) and layer 1 (world)
+		query.CollisionMask = 0b_00001001;
+		PhysicsDirectSpaceState3D space_state = GetWorld3D().DirectSpaceState;
+		Godot.Collections.Dictionary result = space_state.IntersectRay(query);
+		if (result.Count == 0)
+		{
+			return;
+		}
+		Node3D collider = (Node3D) result["collider"];
+		if (collider is InteractArea interact_area)
+		{
+			globals.hovered_interactable = interact_area.interactable;
+		}
+		else
+		{
+			globals.hovered_interactable = null;
+		}
 	}
 }
