84291f099645fc1987778f402f23ddaf797250ea
tobungle <tsan03@outlook.com>
2026-08-09 16:31:56 +0100

    Added item data loading

--- Globals.cs
+++ Globals.cs
@@ -7,9 +7,22 @@ public partial class Globals : Node
 {
 	public double world_item_spin_timer = 0.0;
 	public Interactable hovered_interactable;
+	public Godot.Collections.Dictionary<Variant, Variant> item_data;
+
+    public override void _Ready()
+    {
+		LoadItemData();
+    }
 
 	public override void _PhysicsProcess(double delta)
 	{
 		world_item_spin_timer += delta * WorldItem.spin_rate;
+	}
+
+	void LoadItemData()
+	{
+		Json json = GD.Load<Json>("res://items.json");
+		string json_string = Json.Stringify(json.Data);
+		item_data = (Godot.Collections.Dictionary<Variant, Variant>) GD.StrToVar(json_string);
 	}
 }
